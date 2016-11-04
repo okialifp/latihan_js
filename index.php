290737e539d7e2a0dbba851a867dd3f7f4605037
ferdiansyah <mohamad.ferdiansyah17@gmail.com>
2016-11-04 13:39:52 +0700

    belajar alert

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<Center>
+<label id="label1"></label>
+<br>
+<input type="text" name="data" id="input1">
+<br>
+<textarea type="text" name="textarea id="textarea1"></textarea>
+<br>
+<button onclick="tampil()"> Hai</button>
+	<script>
+
+
+	function tampil() {
+		document.getElementById('label1').innerHTML='Hellow';
+		document.getElementById('label1').value='Hellow';
+		document.getElementById('textarea').innerHTML='Hellow';
+	}
+//var a=1,b=2,c=3;
+//var ta =a+b+c;
+//console.log(ta);
+//console.log("Hello World");
+//document.write("Hello World");
+//konfirmasi
+//alert('hai');
+ // if(confirm('danger')){
+ // 	alert('ok');
+
+ // }else{
+ // 	alert('are you sure?');
+ // }
+
+
+
+
+	</script>
+</body>
+</html>
